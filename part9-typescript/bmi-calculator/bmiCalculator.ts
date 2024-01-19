a68594c96a822bf4f45c5e969c77d7311a8650ef
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2024-01-19 12:19:50 +0200

    Initialize repo and exercise 9.1

---
+++ part9-typescript/bmi-calculator/bmiCalculator.ts
@@ -0,0 +1,20 @@
+function calculateBmi(height: number, weight: number): string {
+  const heightInMeters = height / 100;
+  const bmi = weight / (heightInMeters * heightInMeters);
+
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi >= 25 && bmi < 30) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+}
+
+const height = 170; // in centimeters
+const weight = 65; // in kilograms
+
+const bmiResult = calculateBmi(height, weight);
+console.log(bmiResult);
