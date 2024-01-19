ab4f8e220936fa33c7ec8a4e14da2a7ee2d1270e
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2024-01-19 15:02:39 +0200

    exercise 9.5

--- part9-typescript/bmi-calculator/index.ts
+++ part9-typescript/bmi-calculator/index.ts
@@ -1,8 +1,30 @@
 import express from "express";
 const app = express();
+import { calculateBmi } from "./bmiCalculator";
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (!height && !weight) {
+    res.status(400).json({
+      error: "Needed parameters missing. Parameters needed: weight and height.",
+    });
+    return;
+  }
+
+  if (isNaN(height) || isNaN(weight)) {
+    res.status(400).json({
+      error: "Malformatted parameters. Height and weight should be numbers.",
+    });
+    return;
+  }
+
+  res.json({ height, weight, calculateBmi: calculateBmi(height, weight) });
 });
 
 const PORT = 3003;
