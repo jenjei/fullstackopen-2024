90dd86d2d7243773e90a2ee0f1d5efb05b471f88
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2024-02-13 21:28:09 +0200

    Exercise 9.23

--- part9-typescript/patientor/frontend/patientor/src/components/PatientInfo/index.tsx
+++ part9-typescript/patientor/frontend/patientor/src/components/PatientInfo/index.tsx
@@ -25,6 +25,19 @@ const PatientInfo = () => {
       {patient?.gender === "other" && <Transgender />}
       <p>ssn: {patient?.ssn}</p>
       <p>occupation: {patient?.occupation}</p>
+      <h3>entries</h3>
+      {patient?.entries.map((entry) => (
+        <div key={entry?.id}>
+          <p>
+            {entry?.date} <i>{entry?.description}</i>
+          </p>
+          <ul>
+            {entry?.diagnosisCodes?.map((code) => (
+              <li key={code}>{code}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
